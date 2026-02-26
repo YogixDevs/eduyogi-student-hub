c495f7da99f243180f7f92638554d8353b564e71
Shrigovinda25 <147130287+Shrigovinda25@users.noreply.github.com>
2026-02-27 00:01:17 +0530

    Final fix for Vercel chatbot connection

--- server/server.js
+++ server/server.js
@@ -25,13 +25,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      return callback(new Error('CORS blocked'), false);
-    }
-    return callback(null, true);
-  },
+  origin: true,
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
 }));
